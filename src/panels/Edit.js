c7cd26f4a4b76f551885506a638d00781822f7f7
hapsidra <hapsidra@gmail.com>
2020-09-18 02:31:46 +0300

    fix

--- src/panels/Edit.js
+++ src/panels/Edit.js
@@ -1,9 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Panel, PanelHeader, Div, Text, Button, PanelHeaderBack } from "@vkontakte/vkui";
+import {
+  Panel,
+  PanelHeader,
+  Div,
+  Text,
+  Button,
+  PanelHeaderBack,
+} from "@vkontakte/vkui";
 import Icon56AddCircleOutline from "@vkontakte/icons/dist/56/add_circle_outline";
 
-const Edit = ({ id, image, setImage, title, setTitle, audio, onBack, }) => {
+const Edit = ({
+  id,
+  image,
+  title,
+  audio,
+  description,
+  audioObject,
+  audioInfo,
+  onBack,
+}) => {
   return (
     <Panel id={id}>
       <PanelHeader left={<PanelHeaderBack onClick={onBack} />}>
